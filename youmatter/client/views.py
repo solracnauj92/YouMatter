73d9e801b787d8ddd1c642e6ee042a75983d999a
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2023-11-12 00:55:47 +0000

    Creat views, base , index , community page

--- youmatter/client/views.py
+++ youmatter/client/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class Index(View):
+    def get(self, request, *arg, **kwargs):
+        return render(request, 'client/index.html')
+
+class About(View):
+    def get(self, request, *arg, **kwargs):
+        return render(request, 'client/community.html')
+
